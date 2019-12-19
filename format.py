0ee5f65f0440b6df4a8286944fe61e05b2c1a2e4
Kayo Yin <44864455+kayoyin@users.noreply.github.com>
2019-12-19 23:53:49 +0800

    Update format.py

--- format.py
+++ format.py
@@ -1,11 +1,4 @@
 import json
-import matplotlib.pyplot as plt
-import cv2
-import numpy as np
-import glob
-
-from pycocotools.coco import COCO
-from pycocotools.cocoeval import COCOeval
 
 filename = "submission.json"
 with open(filename, 'r') as file:
